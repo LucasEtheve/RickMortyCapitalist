fb137120d7155f9bb94b489ecb7f8121eb91bc65
LucasEtheve <57803978+LucasEtheve@users.noreply.github.com>
2021-02-03 08:30:29 +0100

    backend

---
+++ StarWarsCapitalist/src/main/java/com/example/StarWarsCapitalist/StarWarsCapitalistApplication.java
@@ -0,0 +1,13 @@
+package com.example.StarWarsCapitalist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsCapitalistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsCapitalistApplication.class, args);
+	}
+
+}
